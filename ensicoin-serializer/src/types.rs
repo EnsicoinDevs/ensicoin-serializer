9002d193883a0ae4adc164ee25ba5d50db875712
Quentin Boyer <qbsecond@gmail.com>
2019-05-19 22:14:55 +0200

    fixed hash

--- ensicoin-serializer/src/types.rs
+++ ensicoin-serializer/src/types.rs
@@ -9,5 +9,19 @@ pub type Sha256Result = generic_array::GenericArray<u8, typenum::U32>;
 
 pub fn hash_to_string(hash: &Sha256Result) -> String {
     hash.iter()
-        .fold(String::new(), |acc, b| format!("{}{:x}", acc, b))
+        .fold(String::new(), |acc, b| format!("{}{:02x}", acc, b))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::hash_to_string;
+    use crate::Sha256Result;
+
+    #[test]
+    fn zero_hash() {
+        assert_eq!(
+            hash_to_string(&Sha256Result::from([0; 32])),
+            String::from("0000000000000000000000000000000000000000000000000000000000000000")
+        )
+    }
 }
